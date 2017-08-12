f1ef2d9a26097efcdfdb34928e90fff48740040b
Matthias Werner <mwerner@informatik.tu-chemnitz.de>
2017-08-12 22:15:07 +0200

    let's have fun

--- kernel/src/panic.rs
+++ kernel/src/panic.rs
@@ -28,7 +28,7 @@ pub extern "C" fn _Unwind_Resume() -> ! {
 pub extern fn rust_begin_panic(msg: core::fmt::Arguments,
                                file: &'static str,
                                line: u32) -> ! {
-    ::debug::fkprintc(format_args!("\nKernel Panic!\n"),::debug::kprint::RED);
+    ::debug::fkprintc(format_args!("Colonel Panic meldet sich zur Stelle!\n"),::debug::kprint::RED);
     ::debug::fkprint(msg);
     ::debug::fkprint(format_args!(" @{}, Zeile: {}",file,line));
     loop{}
