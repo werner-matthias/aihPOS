aa47de63e50efc31b72203f697feb5243e7e0bea
Matthias Werner <mwerner@cs.tu-chemnitz.de>
2017-08-14 15:21:29 +0200

    remove init marker

--- kernel/src/sync/no_concurrency.rs
+++ kernel/src/sync/no_concurrency.rs
@@ -1,9 +1,7 @@
 use core::cell::UnsafeCell;
-use core::sync::atomic::{AtomicBool,Ordering};
 
 pub struct NoConcurrency<T: Sized> {
     data: UnsafeCell<T>,
-    first: AtomicBool,
 }
 
 unsafe impl<T: Sized + Send> Sync for NoConcurrency<T> {}
@@ -13,18 +11,15 @@ impl<'b, T> NoConcurrency<T>{
     pub const fn new(data: T) -> NoConcurrency<T> {
         NoConcurrency{
             data: UnsafeCell::new(data),
-            first: AtomicBool::new(true)
         }
     }
 
     pub fn set(&self, data: T) {
-        self.first.store(false,Ordering::Relaxed);
         let r = self.data.get();
         unsafe { *r = data;}
     }
 
     pub fn get(&self) -> &mut T {
-        self.first.store(false,Ordering::Relaxed);
         let r =self.data.get();
         unsafe { &mut (*r)}
     }
