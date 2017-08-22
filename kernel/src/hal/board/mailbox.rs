c9078d8bf5af2051cde94be2ab4acb9931170626
Matthias Werner <mwerner@cs.tu-chemnitz.de>
2017-08-22 11:45:16 +0200

    bug remove
    - mailbox read hung sometimes

--- kernel/src/hal/board/mailbox.rs
+++ kernel/src/hal/board/mailbox.rs
@@ -3,10 +3,12 @@ use core::mem::transmute;
 
 // Basisadresse des Mailregisters
 const MAILBOX_BASE: u32 = 0x2000B880;
- 
+
+/// Kanäle des Mailbox-Interfaces
 #[allow(dead_code)]
 #[derive(Clone,Copy)]
 #[repr(u32)]
+#[allow(missing_docs)]
 pub enum Channel {
     PowerManagement = 0,
     Framebuffer,
@@ -26,37 +28,40 @@ const MAILBOX_EMPTY: u32 = 1 << 30;
 #[allow(dead_code)]
 #[repr(C)]
 pub struct Mailbox {
-    pub read:    u32,      // 0x00
+    read:    u32,      // 0x00
     _unused: [u32; 3],     // 0x04 0x08 0x0C
-    pub poll:    u32,      // 0x10 
-    pub sender:  u32,      // 0x14
-    pub status:  u32,      // 0x18
-    pub configuration: u32,// 0x1C
-    pub write:   u32,      // 0x20 Mailbox 1!
+    poll:    u32,      // 0x10 
+    sender:  u32,      // 0x14
+    status:  u32,      // 0x18
+    configuration: u32,// 0x1C
+    write:   u32,      // 0x20 Mailbox 1!
 }
 
 impl Mailbox {
+    /// Schreibt in den Kanal `channel` die Daten, die an der Adresse `addr` zu finden sind.
     pub fn write(&mut self, channel: Channel, addr: u32) {
         assert!(addr & 0x0Fu32 == 0);
-        /*if addr & 0x0Fu32 != 0 {
-            blink::blink(blink::BS_SOS);
-        }
-         */
         loop{ 
             if unsafe{volatile_load(&mut self.status)} & MAILBOX_FULL == 0 { break }; 
         }
         self.write =addr | channel as u32; 
     }
 
+    /// Liest die Antwort aus Kanal `channel`
     pub fn read(&mut self, channel: Channel) -> u32 {
-        loop {
-            if (unsafe{volatile_load(&mut self.status)} & MAILBOX_EMPTY == 0) && (self.read & 0xF == channel as u32)
-                { break };
+        let mut ret = !0;
+        while ret & 0xF != channel as u32 {
+            while (unsafe{volatile_load::<u32>(&self.status as *const _ )} & MAILBOX_EMPTY) != 0 {}
+            ret = unsafe{volatile_load::<u32>(&self.read as *const _)};
         }
-        self.read >> 4 
+        ret >> 4
     }
 }
 
+/// Gibt die Mailbox `nr`
+///
+/// # Anmerkung
+/// Es ist nur Mailbox 0 implementiert, alle anderen Nummern erzeugen eine Panik.
 pub fn mailbox(nr: u8) -> &'static mut Mailbox {
     match nr{
         0 => unsafe{ transmute(MAILBOX_BASE)},
