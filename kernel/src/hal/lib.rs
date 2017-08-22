d82512e145b58fb8c41f8cb4bedb848b1cd04f5f
Matthias Werner <mwerner@cs.tu-chemnitz.de>
2017-08-22 22:55:13 +0200

    docu

--- kernel/src/hal/lib.rs
+++ kernel/src/hal/lib.rs
@@ -1,3 +1,4 @@
+#![warn(missing_docs)]
 #![no_std]
 #![feature(
     asm,                      // Assembler in Funktionen...
@@ -5,8 +6,17 @@
     core_intrinsics,          // Nutzung der Intrinsics der Core-Bibliothek
     repr_align,               // Alignment
 )]
+//! Wrapper für Low-Level-Funktionen des Raspberry Pi.
+
 extern crate bit_field;
 extern crate paging;
 
-pub mod board;
+/// Low-Level-Funktionen der ARM-CPU.
+///
+/// # Anmerkung
+/// Alle `struct` haben nur assoziierte Methoden und dienen damit als
+/// Interface für die Hardware. Die Zustände werden direkt in der Hardware
+/// gespeichert.
 pub mod cpu;
+/// Interfaces für den Zugriff auf einige Funktionen des BCM2835 SoC
+pub mod board;
