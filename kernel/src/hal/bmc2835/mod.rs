f27d03850436d69521aaca8d5d9dfac20d335497
Matthias Werner <matthias.werner@cs.tu-chemnitz.de>
2017-09-08 14:55:39 +0200

    UART

--- kernel/src/hal/bmc2835/mod.rs
+++ kernel/src/hal/bmc2835/mod.rs
@@ -1,6 +1,9 @@
 mod uart;
+pub use self::uart::*;
 mod aux;
-pub use self::aux::{MiniUart,MiniUartEnable};
+pub use self::aux::{MiniUart};
+mod pl011;
+pub use self::pl011::{Pl011,Pl011Interrupt};
 mod gpio;
 pub use self::gpio::{Gpio,GpioPinFunctions,GpioPull,GpioEvent,gpio_config};
 mod system_timer;
